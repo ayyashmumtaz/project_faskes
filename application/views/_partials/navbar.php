9575029693f368247521f1319cebdf2e058a3d3b
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2022-06-28 15:47:25 +0700

    delete unused file

--- application/views/_partials/navbar.php
+++ application/views/_partials/navbar.php
@@ -4,7 +4,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+FASKES DEPOK
   </div>
 
   <!-- Navbar -->
@@ -149,19 +149,19 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <center>
+         <span class="brand-text font-weight-light">FASKES DEPOK</span>
+      </center>
+     
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
+   
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block"><?= $this->session->userdata('username');?></a>
         </div>
       </div>
 
