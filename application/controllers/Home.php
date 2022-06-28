db210a11a7c7ca0758a3e26e08abb925b1aee7b9
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2022-06-28 16:28:00 +0700

    login with security & logo faskes

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,6 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$status = $this->session->userdata('role');
+    	if(!isset($status)){
+      redirect(base_url("Login"));
+    }
+	}
+
 	public function index()
 	{
 		$this->load->view('_partials/header');
--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,7 +5,8 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
-		
+		$this->load->view('_partials/header');
+		$this->load->view('login');
 	}
 
 }
