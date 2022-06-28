b991f823cd4b90507c7218c755b8dd5c04ddc5d6
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2022-06-28 16:32:13 +0700

    login function

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -9,6 +9,47 @@ class Login extends CI_Controller {
 		$this->load->view('login');
 	}
 
+	function aksi_login(){
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$data = array(
+			'username' => $username,
+			'password' => md5(str)$password
+			);
+	$hasil = $this->Login_model->cek_login($data);
+	if ($hasil->num_rows() == 1) {
+
+		foreach ($hasil->result() as $sess) {
+		$sess_data['logged_in'] = 'Sudah Login';
+		$sess_data['nama'] = $sess->nama;
+		$sess_data['username'] = $sess->username;
+		$sess_data['role'] = $sess->role;
+		$this->session->set_userdata($sess_data);
+		}
+	$status = $this->session->userdata('role');
+	if ($status == 1) 
+		 {
+			$this->session->set_flashdata('login_berhasil', ' ');
+			redirect(base_url("Beranda"));
+		}elseif ($status == 2) {
+			$this->session->set_flashdata('login_berhasil', ' ');
+			redirect(base_url("Spk/a3"));
+		}elseif ($status == 3) {
+			$this->session->set_flashdata('login_berhasil', ' ');
+			redirect(base_url("Gudang"));
+		}elseif ($status == 4) {
+			$this->session->set_flashdata('login_berhasil', ' ');
+			redirect(base_url("Spk/a3"));
+		}
+	}
+}
+	
+
+	function logout(){
+		$this->session->sess_destroy();
+		redirect(base_url('Login'));
+	}
+
 }
 
 /* End of file Login.php */
