d76bf688dfecf6a4e472b841f989a15b14884cee
adityamusyaffa <aditmusyaffa7@gmail.com>
2022-06-29 21:53:49 +0700

    Change header title SIFASKES Depok to title variable

--- application/views/frontend/layout/header.php
+++ application/views/frontend/layout/header.php
@@ -17,5 +17,5 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Frontend CSS -->
   <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
-  <title>SIFASKES Depok</title>
+  <title><?= $title ?></title>
 </head>
