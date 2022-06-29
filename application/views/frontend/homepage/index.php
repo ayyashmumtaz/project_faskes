fe638c21f5a354032d2998ad983ede924f710b43
adityamusyaffa <aditmusyaffa7@gmail.com>
2022-06-29 21:54:14 +0700

    Add CTA

--- application/views/frontend/homepage/index.php
+++ application/views/frontend/homepage/index.php
@@ -8,22 +8,22 @@
             <i class="bi bi-list"></i>
           </button>
           <div class="collapse navbar-collapse justify-content-between" id="navbarResponsive">
-            <ul class="navbar-nav me-4 my-3 my-lg-0 navv">
+            <ul class="navbar-nav my-3 my-lg-0 mx-auto">
               <li class="nav-item"><a class="nav-link me-lg-3 active" href="#Home">Home</a></li>
               <li class="nav-item"><a class="nav-link me-lg-3" href="#Articles">Articles</a></li>
               <li class="nav-item"><a class="nav-link me-lg-3" href="#About">About</a></li>
             </ul>
             <div>
-              <button class="btn btn-outline-primary rounded-pill px-3 mb-2 mb-lg-0">
+              <a href="<?= base_url() ?>login" class="btn btn-outline-primary rounded-pill px-3 mb-2 mb-lg-0">
                 <span class="d-flex align-items-center">
                   <span class="small">Masuk</span>
                 </span>
-              </button>
-              <button class="btn btn-primary rounded-pill px-3 mb-2 ms-2 mb-lg-0">
+              </a>
+              <a href="<?= base_url() ?>login/register" class="btn btn-primary rounded-pill px-3 mb-2 ms-2 mb-lg-0">
                 <span class="d-flex align-items-center">
                   <span class="small">Daftar</span>
                 </span>
-              </button>
+              </a>
             </div>
           </div>
         </div>
@@ -32,10 +32,13 @@
       <!-- Header-->
       <div class="container">
         <div class="row hero rounded-5">
-          <div class="col-sm-6">
-            <h1 class="text-hero fs-2 ms-sm-4 my-5"><span class="non-bold">Temukan</span> <br> Fasilitas Kesehatan Depok<br> <span class="non-bold">Semau Anda</span></h1>
+          <div class="col-md-7">
+            <div class="row row-cols-sm-1 mt-5 txt-cta-hero">
+              <h1 class="text-hero fs-5 mx-auto text-md-start"><span class="non-bold">Temukan</span> <br> Fasilitas Kesehatan Depok<br> <span class="non-bold">Sesuka Anda</span></h1>
+              <a href="<?= base_url() ?>/browse/browseAll" class="btn btn-primary ms-md-4 cta mt-2 mx-auto">Lihat Semua</a>
+            </div>
           </div>
         </div>
       </div>
+
     </header>
-  
