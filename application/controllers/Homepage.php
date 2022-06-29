50b91ca422cb25d81a0ee5cac657fce89902cf3e
adityamusyaffa <aditmusyaffa7@gmail.com>
2022-06-29 21:51:30 +0700

    Add title to header

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Homepage extends CI_Controller {
   public function index() {
     $data['title'] = 'SIFASKES';
-    $this->load->view('frontend/layout/header');
+    $this->load->view('frontend/layout/header', $data);
     $this->load->view('frontend/homepage/index', $data);
     $this->load->view('frontend/layout/footer');
   }
