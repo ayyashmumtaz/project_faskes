b8a6e7df77430618ac3dbd6cd241ed3ebdc415cb
adityamusyaffa <aditmusyaffa7@gmail.com>
2022-06-29 09:02:47 +0700

    Create Folder Homepage inside frontend   - Add index.php inside it     - Add Nav     - Add Hero Section

---
+++ application/views/frontend/homepage/index.php
@@ -0,0 +1,41 @@
+  <body>
+    <header>
+      <!-- Navigation-->
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-white" id="mainNav">
+        <div class="container px-5">
+          <a class="navbar-brand fw-bold" href="#page-top">SIFASKES DEPOK</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="bi bi-list"></i>
+          </button>
+          <div class="collapse navbar-collapse justify-content-between" id="navbarResponsive">
+            <ul class="navbar-nav me-4 my-3 my-lg-0 navv">
+              <li class="nav-item"><a class="nav-link me-lg-3 active" href="#Home">Home</a></li>
+              <li class="nav-item"><a class="nav-link me-lg-3" href="#Articles">Articles</a></li>
+              <li class="nav-item"><a class="nav-link me-lg-3" href="#About">About</a></li>
+            </ul>
+            <div>
+              <button class="btn btn-outline-primary rounded-pill px-3 mb-2 mb-lg-0">
+                <span class="d-flex align-items-center">
+                  <span class="small">Masuk</span>
+                </span>
+              </button>
+              <button class="btn btn-primary rounded-pill px-3 mb-2 ms-2 mb-lg-0">
+                <span class="d-flex align-items-center">
+                  <span class="small">Daftar</span>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <!-- Header-->
+      <div class="container">
+        <div class="row hero rounded-5">
+          <div class="col-sm-6">
+            <h1 class="text-hero fs-2 ms-sm-4 my-5"><span class="non-bold">Temukan</span> <br> Fasilitas Kesehatan Depok<br> <span class="non-bold">Semau Anda</span></h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  
